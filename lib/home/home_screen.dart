9f48c61c516ecd1a6c00fe689cbd54d7ed2f7d3e
ahmed <ahmedkhaled00112233445500@gmail.com>
2022-12-29 16:45:18 +0200

    initial project

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName='home screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Scaffold(
+        appBar:AppBar(
+          title:Text('News App'),
+        ),
+      ),
+    );
+  }
+}
